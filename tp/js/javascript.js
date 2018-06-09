4fe8739352b410382f3e9f01db88043592831dc5
JuanDeccechis <juandeccechis@gmail.com>
2018-06-08 22:55:33 -0300

    primer commit. Bootstrap y Partial Render casi terminados.

---
+++ tp/js/javascript.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+	"use-strict";
+
+	function loadHome(event){
+		event.preventDefault();
+		let container = document.querySelector(".cuerpo");
+		container.innerHTML = "<h1>Loading...</h1>";
+		fetch("http://localhost/proyectos/TP/home.html").then(
+			function(response){
+		      		response.text().then(t  => 
+					container.innerHTML = t)
+					});
+	}
+
+	function loadCatedras(event){
+		event.preventDefault();
+		let container = document.querySelector(".cuerpo");
+		container.innerHTML = "<h1>Loading...</h1>";
+		fetch("http://localhost/proyectos/TP/catedras.html").then(
+			function(response){
+		      		response.text().then(t  => 
+					container.innerHTML = t)
+					});
+	}
+
+	function loadCalendario(event){
+		event.preventDefault();
+		let container = document.querySelector(".cuerpo");
+		container.innerHTML = "<h1>Loading...</h1>";
+		fetch("http://localhost/proyectos/TP/calendario.html").then(
+			function(response){
+		      		response.text().then(t  => 
+					container.innerHTML = t)
+					});
+	}
+	window.onload = loadHome;
+	let jshome = document.querySelectorAll(".js-home");
+	jshome.forEach(e=> e.addEventListener("click", loadHome));
+	let jscatedras = document.querySelectorAll(".js-catedras");
+	jscatedras.forEach(e=> e.addEventListener("click", loadCatedras));
+	let jscalendario = document.querySelectorAll(".js-calendario");
+	jscalendario.forEach(e=> e.addEventListener("click", loadCalendario));
+})
