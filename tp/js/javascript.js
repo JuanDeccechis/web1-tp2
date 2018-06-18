1f8376b4920ed10bb54e7723279fb244b3bb5855
JuanDeccechis <juandeccechis@gmail.com>
2018-06-18 15:12:04 -0300

    version con todo

--- tp/js/javascript.js
+++ tp/js/javascript.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 	"use-strict";
+	let seguirActualizando = true;
+	let arregloJsons= [];
+	let arregloNombres = [];
+	let url = "http://web-unicen.herokuapp.com/api/groups/deccechis/prueba/";
+	let contenido = { 
+		"id": "",
+		"nombre": "",
+		"link": ""
+	}
+	let objeto ={
+		"thing": contenido
+	}
+
 
 	function loadHome(event){
 		event.preventDefault();
@@ -9,8 +22,34 @@ document.addEventListener("DOMContentLoaded", function(){
 		fetch("home.html").then(
 			function(response){
 		      		response.text().then(t  => 
-					container.innerHTML = t)
-					});
+						container.innerHTML = t)
+					.catch(error => console.log("Error en loadHome, text(): "+error))
+					})
+			.catch(error => console.log("Error en loadHome: "+error))
+	}
+
+	function loadCatedrasAdmin(event){
+		event.preventDefault();
+		let container = document.querySelector(".cuerpo");
+		container.innerHTML = "<h2>Loading...</h2>";
+		fetch("catedrasAdmin.html").then(
+			function(response){
+	      		response.text().then(t  => {
+				container.innerHTML = t;
+				container.querySelector("#timer").addEventListener("click", comenzarActualizaciones);
+				container.querySelector("#stopTimer").addEventListener("click", detenerActualizaciones);
+				container.querySelector("#js-getId").addEventListener("click", eventGetOne);
+				container.querySelector("#js-get").addEventListener("click", eventGet);
+				container.querySelector("#js-delete-all").addEventListener("click", eventDeleteAll);
+				container.querySelector("#name").addEventListener("keyup", filtrarPorNombre);
+				container.querySelector("#js-create").addEventListener("click", eventCreate);
+				container.querySelector("#js-create-n").addEventListener("click", eventCreateCount);
+
+				get();
+				})
+				.catch(error => console.log("Error en loadCatedrasAdmin, text(): "+error))
+			})
+		.catch(error => console.log("Error en loadCatedrasAdmin: "+error))
 	}
 
 	function loadCatedras(event){
@@ -20,14 +59,27 @@ document.addEventListener("DOMContentLoaded", function(){
 		fetch("catedras.html").then(
 			function(response){
 		      		response.text().then(t  => {
-					container.innerHTML = t;
-					container.querySelector("#js-get").addEventListener("click", eventGet);
-					container.querySelector("#js-create").addEventListener("click", eventCreate);
-					get();					
-					})
-				});
+						container.innerHTML = t;
+						})
+					.catch(error => console.log("Error en loadCatedras, text(): "+error))
+				})
+		.catch(error => console.log("Error en loadCatedras: "+error))
 	}
 
+	function comenzarActualizaciones(e){
+		e.preventDefault();
+		let container = document.querySelector(".cuerpo");
+		let tiempoRecarga = parseInt(container.querySelector("#time-refresh").value)*1000;
+		setInterval(function() {
+			if (seguirActualizando)
+				get();
+		}, tiempoRecarga);
+	}
+
+	function detenerActualizaciones(e){
+		e.preventDefault();
+		seguirActualizando = false;
+	}
 	function loadCalendario(event){
 		event.preventDefault();
 		let container = document.querySelector(".cuerpo");
@@ -35,8 +87,10 @@ document.addEventListener("DOMContentLoaded", function(){
 		fetch("calendario.html").then(
 			function(response){
 		      		response.text().then(t  => 
-					container.innerHTML = t)
-					});
+						container.innerHTML = t)
+					.catch(error => console.log("Error en loadCalendario, text(): "+error))
+					})
+		.catch(error => console.log("Error en loadCalendario: "+error))
 	}
 
 
@@ -46,49 +100,84 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	let jshome = document.querySelectorAll(".js-home");
 	jshome.forEach(e=> e.addEventListener("click", loadHome));
-	let jscatedras = document.querySelectorAll(".js-catedras");
-	jscatedras.forEach(e=> e.addEventListener("click", loadCatedras));
+	let jscatedrasAdmin = document.querySelectorAll(".js-catedras-admin");
+	jscatedrasAdmin.forEach(e=> e.addEventListener("click", loadCatedrasAdmin));
 	let jscalendario = document.querySelectorAll(".js-calendario");
 	jscalendario.forEach(e=> e.addEventListener("click", loadCalendario));
+	let jscatedras = document.querySelectorAll(".js-catedras");
+	jscatedras.forEach(e=> e.addEventListener("click", loadCatedras));
 
+
+	function eventGetOne(e){
+		e.preventDefault();
+		getOne();
+	}
 	function eventGet(e){
 		e.preventDefault();
 		get();
 	}
 	function eventCreate(e){
 		e.preventDefault();
-		let propiedad = document.querySelector("#js-property").value;
-		let nombre = document.querySelector("#js-name").value;
-		if (propiedad && nombre)
-			create(propiedad, nombre);
+		let id = document.querySelector("#id").value;
+		let nombre = document.querySelector("#name").value;
+		let link = document.querySelector("#link").value;
+		if (id && nombre && link){
+			create(id, nombre, link);
+			/*get();*/
+		}
 		else
-			console.log("faltan argumentos");
+			console.log("faltan argumentos para crear una fila completa");
+	}
+	function eventCreateCount(e){
+		e.preventDefault();
+		let id = document.querySelector("#id").value;
+		let nombre = document.querySelector("#name").value;
+		let link = document.querySelector("#link").value;
+		let cantidad = document.querySelector("#count").value;
+		if (id && nombre && link){
+			for (let i = 0; i < cantidad; i++)
+				create(id, nombre, link);
+			/*get();*/
+		}
+		else
+			console.log("faltan argumentos para crear "+ n + " filas completas");
 	}
 	function eventUpdate(e){
 		e.preventDefault();
-		let propiedad = document.querySelector("#js-property").value;
-		let nombre = document.querySelector("#js-name").value;
-		if (propiedad && nombre)
-			update(this, propiedad, nombre);
+		let id = document.querySelector("#id").value;
+		let nombre = document.querySelector("#name").value;
+		let link = document.querySelector("#link").value;
+		if (id && nombre && link){
+			update(this, id, nombre, link);
+			/*get();*/
+		}
 		else
-			console.log("faltan argumentos");
+			console.log("faltan argumentos para actualizar la fila completa");
 	}
 	function eventDelete(e){
 		e.preventDefault();
 		del(this);
+		/*get();*/
+	}
+	function eventDeleteAll(e){
+		e.preventDefault();
+		let  filas = document.querySelectorAll(".btn-delete");
+		for (let i = 0; i < filas.length; i++) {
+			del(filas[i]);
+		}
+		/*get();*/
 	}
 
 	/*** HTTP ***/
 
 	function get(){
-		let url = "http://web-unicen.herokuapp.com/api/groups/deccechis/prueba/";
 		let container = document.querySelector("tbody");
 		container.innerHTML = "Cargando";
 		fetch(url).then(function(r){ 
 			console.log("GET status: " + r.status);
 			r.json()
 				.then(function(json){
-					container.innerHTML = mostrar(container, json);
+					container.innerHTML = mostrar(container, json, "prueba");
 					let deletes = container.querySelectorAll(".btn-delete");
 					let updates = container.querySelectorAll(".btn-update");
 					for (let i = 0; i < deletes.length; i++) {
@@ -96,29 +185,123 @@ document.addEventListener("DOMContentLoaded", function(){
 						updates[i].addEventListener("click", eventUpdate);
 					}
 				})
-				.catch(error => container.innerHTML = "Error")
+				.catch(function(error){
+					error => container.innerHTML = "Error";
+					console.log("Error en GET, json(): " + error);
+				})
+		})
+		.catch(function(error){
+			error => container.innerHTML = "Error";
+			console.log("Error en GET: " + error);
 		})
 	}		
 	
-	function mostrar(container, json){
-		let resultado = "<tr>";
-		for (let i = 0; i < json.prueba.length; i++) {
-			resultado = resultado + "<td>" + json.prueba[i].thing.nombre + "	-	id: " + json.prueba[i]._id + "</td>"
-			resultado = resultado + "<td> 1 </td> <td> </td> <td class='nuevo'><button id='"+json.prueba[i]._id+"' class='btn btn-warning btn-update'> modificar</button> <button id='"+ json.prueba[i]._id +"' class='btn btn-danger btn-delete'> borrar</button> </td>" + "</tr>"
+	function getOne(){
+		let container = document.querySelector("tbody");
+		let idCatedra = document.querySelector("#id").value;
+		let id = "";
+		for (var i = 0; i < arregloJsons.length; i++) {
+			if(arregloJsons[i].thing.id === idCatedra)
+				id = arregloJsons[i]._id;
+		}
+		if(id === "")
+			console.log("Error en GET one, el ID no corresponde con ningún elemento");
+		else{
+		/*let container = document.querySelector("tbody");
+		let id = document.querySelector("#js-id").value; */
+			let urlCompleta = url + id;
+			fetch(urlCompleta).then(function(r){ 
+				console.log("GET status: " + r.status);
+				r.json()
+					.then(function(json){
+						container.innerHTML = mostrar(container, json, "information");
+						let deletes = container.querySelectorAll(".btn-delete");
+						let updates = container.querySelectorAll(".btn-update");
+						for (let i = 0; i < deletes.length; i++) {
+							deletes[i].addEventListener("click", eventDelete);
+							updates[i].addEventListener("click", eventUpdate);
+						}
+					})
+					.catch(function(error){
+						error => container.innerHTML = "Error";
+						console.log("Error en GET one, json(): " + error);
+					})
+			})
+			.catch(function(error){
+				error => container.innerHTML = "Error";
+				console.log("Error en GET one: " + error);
+			})
+		}
+	}
+
+	function filtrarPorNombre(){
+		let container = document.querySelector("tbody");
+		let nombreCatedra = document.querySelector("#name").value;
+		let filtrados = [];
+		let count = 0;
+		for (var i = 0; i < arregloJsons.length; i++) {
+			if(arregloJsons[i].thing.nombre.includes(nombreCatedra)){
+				filtrados[count] = arregloJsons[i];
+				count++;
+			}
+		}
+		if(count !== 0){
+			let container = document.querySelector("tbody");
+			container.innerHTML = mostrar(container, filtrados, "filtro")
+		}
+	}
+
+	function mostrar(container, json, stringContenedor){
+		let resultado = "";
+		if (stringContenedor === "prueba"){
+
+			for (let i = 0; i < json.prueba.length; i++) {
+				if (json.prueba[i].thing.nombre.includes("web"))
+					//las filas resaltadas son las que incluyen "web"
+					resultado = resultado +"<tr class='filaResaltada'>";
+				else
+					resultado = resultado +"<tr>";
+				arregloNombres[i] = json.prueba[i].thing.nombre;
+				arregloJsons[i] = json.prueba[i];
+				resultado = resultado + "<td> " + json.prueba[i].thing.id + " </td>";
+				resultado = resultado + "<td> " + json.prueba[i].thing.nombre + " </td>";
+				resultado = resultado + "<td> " + "<a href='" + json.prueba[i].thing.link + "'><img src='images/icon-link.png' alt='Link'> </a>" + " </td>";
+				resultado = resultado + "<td><button id='"+json.prueba[i]._id+"' class='btn btn-warning btn-update'> modificar</button> <button id='"+ json.prueba[i]._id +"' class='btn btn-danger btn-delete'> borrar</button> </td>" + "</tr>"
+			}
+		}
+		else{
+			if (stringContenedor === "information") {
+				if (json.information.thing.nombre.includes("web"))
+					//las filas resaltadas son las que incluyen "web"
+					resultado = resultado +"<tr class='filaResaltada'>";
+				else
+					resultado = resultado +"<tr>";
+				resultado = resultado + "<td> " + json.information.thing.id + " </td>";
+				resultado = resultado + "<td> " + json.information.thing.nombre + " </td>";
+				resultado = resultado + "<td> " + "<a href='" + json.information.thing.link + "'><img src='images/icon-link.png' alt='Link'> </a>" + " </td>";
+				resultado = resultado + "<td><button id='"+json.information._id+"' class='btn btn-warning btn-update'> modificar</button> <button id='"+ json.information._id +"' class='btn btn-danger btn-delete'> borrar</button> </td>" + "</tr>"
+			}	
+			else{
+				for (let i = 0; i < json.length; i++) {
+					if (json[i].thing.nombre.includes("web"))
+					//las filas resaltadas son las que incluyen "web"
+						resultado = resultado +"<tr class='filaResaltada'>";
+					else
+						resultado = resultado +"<tr>";
+					resultado = resultado + "<td> " + json[i].thing.id + " </td>";
+					resultado = resultado + "<td> " + json[i].thing.nombre + " </td>";
+					resultado = resultado + "<td> " + "<a href='" + json[i].thing.link + "'><img src='images/icon-link.png' alt='Link'> </a>" + " </td>";
+					resultado = resultado + "<td><button id='"+json[i]._id+"' class='btn btn-warning btn-update'> modificar</button> <button id='"+ json[i]._id +"' class='btn btn-danger btn-delete'> borrar</button> </td>" + "</tr>"
+				}
+			}
 		}
 		return resultado;
 	}
 
-	function create(propiedad, nombre){
-		let url = "http://web-unicen.herokuapp.com/api/groups/deccechis/prueba/";
-		let objeto = { 
-			"propiedad": propiedad,
-			"nombre": nombre
-		}
-		let equipo ={
-			"thing": objeto
-		}
-		
+	function create(propiedad, nombre, link){
+		contenido.id = propiedad;
+		contenido.nombre = nombre;
+		contenido.link = link;
 		let container = document.querySelector("#resultado");
 		container.innerHTML = "Cargando";
 		fetch(url, {
@@ -126,23 +309,25 @@ document.addEventListener("DOMContentLoaded", function(){
 	        "headers": {
 	            'Content-Type': 'application/json'
 	        },
-	        "body": JSON.stringify(equipo)
+	        "body": JSON.stringify(objeto)
 	    })
-	    .then(function(r){
-	    	console.log("POST status: " + r.status);
-	    	container.innerHTML = "";
-	    	get();
-	    })
-	    .catch(error => console.log(error))
+		    .then(function(r){
+		    	console.log("POST status: " + r.status);
+		    	container.innerHTML = "";
+		    	get();
+		    })
+	    .catch(function(error){
+			console.log("Error en CREATE: " + error);
+		})
 	}
 
 
 	function del(btn){
 		console.log(btn);
-		let url = "http://web-unicen.herokuapp.com/api/groups/deccechis/prueba/" + btn.id;
+		let urlCompleta = url + btn.id;
 		let container = document.querySelector("#resultado");
 		container.innerHTML = "Cargando";
-		fetch(url, {
+		fetch(urlCompleta, {
 	        "method": 'DELETE',
 	        "headers": {
 	            'Content-Type': 'application/json'
@@ -153,35 +338,34 @@ document.addEventListener("DOMContentLoaded", function(){
 	    	container.innerHTML = "";
 	    	get();
 	    })
-	    .catch(error => console.log(error))
+	    .catch(function(error){
+			console.log("Error en DELETE: " + error);
+		})
 	}
 
-	function update(btn, propiedad, nombre){
-		let url = "http://web-unicen.herokuapp.com/api/groups/deccechis/prueba/" + btn.id;
+	function update(btn, propiedad, nombre, link){
+		let urlCompleta = url + btn.id;
 		let container = document.querySelector("#resultado");
 		container.innerHTML = "Cargando";
 
-		let objeto = { 
-			"propiedad": propiedad,
-			"nombre": nombre
-		}
-		let equipo ={
-			"thing": objeto
-		}
-
-		fetch(url, {
+		contenido.id = propiedad;
+		contenido.nombre = nombre;
+		contenido.link = link;
+		fetch(urlCompleta, {
 	        "method": 'PUT',
 	        "headers": {
 	            'Content-Type': 'application/json'
 	        },
-	        "body": JSON.stringify(equipo)
+	        "body": JSON.stringify(objeto)
 	    })
 	    .then(function(r){
 	    	console.log("PUT status: " + r.status);
 	    	container.innerHTML = "";
 	    	get();
 	    })
-	    .catch(error => console.log(error))
+	    .catch(function(error){
+			console.log("Error en UPDATE: " + error);
+		})
 	}
 
 })
