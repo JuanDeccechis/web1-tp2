1c7b12a82984a685ed7ca13b7747f433a8feb7a9
JuanDeccechis <juandeccechis@gmail.com>
2018-06-21 15:44:05 -0300

    ultima version. Agregar cambios de Andres

--- tp/js/javascript.js
+++ tp/js/javascript.js
@@ -17,6 +17,8 @@ document.addEventListener("DOMContentLoaded", function(){
 	/*	Esta función realiza el PARTIAL RENDER que carga en la sección "cuerpo" del index, el home de la página.	*/
 	function loadHome(event){
 		event.preventDefault();
+		if (seguirActualizando)
+			seguirActualizando = false;
 		let container = document.querySelector(".cuerpo");
 		container.innerHTML = "<h2>Cargando...</h2>";
 		fetch("home.html").then(
@@ -40,12 +42,13 @@ document.addEventListener("DOMContentLoaded", function(){
 				container.innerHTML = t;
 				container.querySelector("#timer").addEventListener("click", comenzarActualizaciones);
 				container.querySelector("#stopTimer").addEventListener("click", detenerActualizaciones);
+				
 				container.querySelector("#js-getId").addEventListener("click", eventGetOne);
 				container.querySelector("#js-get").addEventListener("click", eventGet);
 				container.querySelector("#js-delete-all").addEventListener("click", eventDeleteAll);
 				container.querySelector("#name").addEventListener("keyup", filtrarPorNombre);
-				container.querySelector("#js-create").addEventListener("click", eventCreate);
-				container.querySelector("#js-create-n").addEventListener("click", eventCreateCount);
+				container.querySelector("#js-create").addEventListener("click", eventInsertOne);
+				container.querySelector("#js-create-n").addEventListener("click", eventInsertCount);
 
 				get();
 				})
@@ -57,6 +60,8 @@ document.addEventListener("DOMContentLoaded", function(){
 	/*	Esta función realiza el PARTIAL RENDER que carga en la sección "cuerpo" del index, la tabla de cátedras de la página.	*/
 	function loadCatedras(event){
 		event.preventDefault();
+		if (seguirActualizando)
+			seguirActualizando = false;
 		let container = document.querySelector(".cuerpo");
 		container.innerHTML = "<h2>Cargando...</h2>";
 		fetch("catedras.html").then(
@@ -72,6 +77,8 @@ document.addEventListener("DOMContentLoaded", function(){
 	/*	Esta función realiza el PARTIAL RENDER que carga en la sección "cuerpo" del index, el calendario de la página.	*/
 	function loadCalendario(event){
 	event.preventDefault();
+	if (seguirActualizando)
+		seguirActualizando = false;
 	let container = document.querySelector(".cuerpo");
 	container.innerHTML = "<h2>Cargando...</h2>";
 	fetch("calendario.html").then(
@@ -138,25 +145,39 @@ document.addEventListener("DOMContentLoaded", function(){
 	}
 
 	/*	Llama a crear fila si todos los campos necesarios tienen datos.	*/
+	function eventInsertOne(e){
+		e.preventDefault();
+		let container = document.querySelector("tbody"); 
+		let colTr = insertarFilaVacia(null, 0, 1);
+		container.appendChild(colTr);
+	}
+
+	function eventInsertCount(e){
+		e.preventDefault();
+		let container = document.querySelector("tbody"); 
+		let colTr = insertarFilaVacia(null, 0, 2);
+		container.appendChild(colTr);
+	}
+
 	function eventCreate(e){
 		e.preventDefault();
-		let id = document.querySelector("#id").value;
-		let nombre = document.querySelector("#name").value;
-		let link = document.querySelector("#link").value;
+		let id = document.querySelectorAll(".nuevaFilaId")[0].value;
+		let nombre = document.querySelectorAll(".nuevaFilaNombre")[0].value;
+		let link = document.querySelectorAll(".nuevaFilaLink")[0].value;
 		if (id && nombre && link){
 			create(id, nombre, link);
-			/*get();*/
+		//	get();
 		}
 		else
-			console.log("faltan argumentos para crear una fila completa");
+			console.log("faltan argumentos para crear una fila completa"); 
 	}
 
 	/*	Llama varias veces a crear fila si todos los campos necesarios tienen datos.	*/
 	function eventCreateCount(e){
 		e.preventDefault();
-		let id = document.querySelector("#id").value;
-		let nombre = document.querySelector("#name").value;
-		let link = document.querySelector("#link").value;
+		let id = document.querySelectorAll(".nuevaFilaId")[0].value;
+		let nombre = document.querySelectorAll(".nuevaFilaNombre")[0].value;
+		let link = document.querySelectorAll(".nuevaFilaLink")[0].value;
 		let cantidad = document.querySelector("#count").value;
 		if (id && nombre && link){
 			for (let i = 0; i < cantidad; i++)
@@ -164,21 +185,29 @@ document.addEventListener("DOMContentLoaded", function(){
 			/*get();*/
 		}
 		else
-			console.log("faltan argumentos para crear "+ n + " filas completas");
+			console.log("faltan argumentos para crear "+ cantidad + " filas completas");
 	}
 
 	/*	Llama a actualizar fila si todos los campos necesarios tienen datos.	*/
 	function eventUpdate(e){
 		e.preventDefault();
-		let id = document.querySelector("#id").value;
-		let nombre = document.querySelector("#name").value;
-		let link = document.querySelector("#link").value;
+		let id = document.querySelectorAll(".nuevaFilaId")[0].value;
+		let nombre = document.querySelectorAll(".nuevaFilaNombre")[0].value;
+		let link = document.querySelectorAll(".nuevaFilaLink")[0].value;
 		if (id && nombre && link){
 			update(this, id, nombre, link);
 			/*get();*/
 		}
 		else
 			console.log("faltan argumentos para actualizar la fila completa");
+	}
+
+	function eventPreUpdate(e){
+		let container = document.querySelector("tbody");
+		let numFila = this.getAttribute("data-numeroFila");
+		let colTr = insertarFilaVacia(arregloJsons, numFila, 3);
+		container.removeChild(container.childNodes[numFila]);
+		container.appendChild(colTr);
 	}
 
 	function eventDelete(e){
@@ -213,7 +242,7 @@ document.addEventListener("DOMContentLoaded", function(){
 					let updates = container.querySelectorAll(".btn-update");
 					for (let i = 0; i < deletes.length; i++) {
 						deletes[i].addEventListener("click", eventDelete);
-						updates[i].addEventListener("click", eventUpdate);
+						updates[i].addEventListener("click", eventPreUpdate);
 					}
 				})
 				.catch(function(error){
@@ -253,7 +282,7 @@ document.addEventListener("DOMContentLoaded", function(){
 						let updates = container.querySelectorAll(".btn-update");
 						for (let i = 0; i < deletes.length; i++) {
 							deletes[i].addEventListener("click", eventDelete);
-							updates[i].addEventListener("click", eventUpdate);
+							updates[i].addEventListener("click", eventPreUpdate);
 						}
 					})
 					.catch(function(error){
@@ -286,31 +315,36 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 
-	/*	Esta función incluye el HTML necesario para visualizar de manera correcta las filas a mostrar en cada caso, de la tabla
-	 de la página. Se debe respetar la estructura de JSONs, por eso hay distintas formas.	*/
+	/*	Esta función permite visualizar de manera correcta las filas a mostrar en cada caso, de la tabla
+	 de la página.*/
 	function mostrar(container, json, stringContenedor){
 		let resultado = "";
+		let numFila = 0;
 		if (stringContenedor === "prueba")
 			for (let i = 0; i < json.catedra.length; i++) {
 				arregloJsons[i] = json.catedra[i];
-				let colTr = crearFila(json.catedra[i]);
+				let colTr = crearFila(json.catedra[i], numFila);
 				container.appendChild(colTr);
+				numFila++;
 			}
 		else{
 			if (stringContenedor === "information") {
-				let colTr = crearFila(json.information);
+				let colTr = crearFila(json.information, numFila);
 				container.appendChild(colTr);
+				numFila++;
 			}	
 			else
 				for (let i = 0; i < json.length; i++) {
-					let colTr = crearFila(json[i]);
+					let colTr = crearFila(json[i], numFila);
 					container.appendChild(colTr);
+					numFila++;
 				}
 		}
 		return resultado;
 	}
 
-	function crearFila(json){
+	/* Esta función incluye el HTML necesario para la creación de filas de la tabla. */
+	function crearFila(json, numFila){
 		let colTr = document.createElement("tr");
 		if (json.thing.nombre.includes("web")){
 			//las filas resaltadas son las que incluyen "web"
@@ -335,12 +369,13 @@ document.addEventListener("DOMContentLoaded", function(){
 
 		let colBotones = document.createElement("td");
 		let botonUpdate = document.createElement("button");
-		botonUpdate.setAttribute("id", json._id);
+		botonUpdate.setAttribute("data-fila", json._id);
+		botonUpdate.setAttribute("data-numeroFila", numFila);
 		botonUpdate.classList.add("btn", "btn-warning", "btn-update");
 		let contenidoUpdate = document.createTextNode("Modificar");
 		botonUpdate.appendChild(contenidoUpdate);
 		let botonDelete = document.createElement("button");
-		botonDelete.setAttribute("id", json._id);
+		botonDelete.setAttribute("data-fila", json._id);
 		botonDelete.classList.add("btn", "btn-danger", "btn-delete");
 		let contenidoDelete = document.createTextNode("Eliminar");
 		botonDelete.appendChild(contenidoDelete);
@@ -351,6 +386,55 @@ document.addEventListener("DOMContentLoaded", function(){
 		colTr.appendChild(colNombre);
 		colTr.appendChild(colLink);
 		colTr.appendChild(colBotones);
+		return colTr;
+	}
+
+	function insertarFilaVacia(json,numFila, cantidad){
+		let colBoton = document.createElement("td");
+		let contenidoBoton = document.createElement("button");
+		contenidoBoton.classList.add("btn", "btn-primary", "btn-guardar");
+		let textoBoton = document.createTextNode("Guardar");
+		contenidoBoton.appendChild(textoBoton);
+		let placeholderId = "id...";
+		let placeholderNombre = "nombre...";
+		let placeholderLink = "link...";
+		if (cantidad === 1)
+			contenidoBoton.addEventListener("click", eventCreate);
+		if (cantidad === 2)
+			contenidoBoton.addEventListener("click", eventCreateCount);
+		if (cantidad === 3){
+			contenidoBoton.addEventListener("click", eventUpdate);
+			contenidoBoton.setAttribute("data-fila", json[numFila]._id);
+			placeholderId = json[numFila].thing.id;
+			placeholderNombre = json[numFila].thing.nombre;
+			placeholderLink = json[numFila].thing.link;
+		}
+
+		let colTr = document.createElement("tr");
+		let colId = document.createElement("td");
+		let contenidoId = document.createElement("input");
+		contenidoId.setAttribute("placeholder", placeholderId);
+		contenidoId.classList.add("nuevaFilaId");
+		colId.appendChild(contenidoId);
+		colTr.appendChild(colId);
+
+		let colNombre = document.createElement("td");
+		let contenidoNombre = document.createElement("input");
+		contenidoNombre.setAttribute("placeholder", placeholderNombre);
+		contenidoNombre.classList.add("nuevaFilaNombre");
+		colNombre.appendChild(contenidoNombre);
+		colTr.appendChild(colNombre);
+
+		let colLink = document.createElement("td");
+		let contenidoLink = document.createElement("input");
+		contenidoLink.setAttribute("placeholder", placeholderLink);
+		contenidoLink.classList.add("nuevaFilaLink");
+		colLink.appendChild(contenidoLink);
+		colTr.appendChild(colLink);
+
+		colBoton.appendChild(contenidoBoton);
+		colTr.appendChild(colBoton);
+		
 		return colTr;
 	}
 
@@ -392,7 +476,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	/*	Esta función realiza el DELETE HTTP de la fila de la página.	*/
 	function del(btn){
-		let urlCompleta = url + btn.id;
+		let urlCompleta = url + btn.getAttribute("data-fila");
 		let container = document.querySelector("#resultado");
 		let cantHijos = container.childNodes.length;
 		let count = 0;
@@ -425,7 +509,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	/*	Esta función realiza el UPDATE HTTP de la fila, con el contenido de los inputs de la página.	*/
 	function update(btn, propiedad, nombre, link){
-		let urlCompleta = url + btn.id;
+		let urlCompleta = url + btn.getAttribute("data-fila");
 		let container = document.querySelector("#resultado");
 		let cantHijos = container.childNodes.length;
 		let count = 0;
